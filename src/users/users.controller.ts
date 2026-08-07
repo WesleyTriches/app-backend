586fc40442e0e1715361b0c00752bae4aed2b08c
Wesley Triches <wesleygtriches@gmail.com>
2026-08-07 00:05:51 -0300

    feat: adiciona configuração do Prisma e rota GET de usuários

---
+++ src/users/users.controller.ts
@@ -0,0 +1,25 @@
+import { Controller, Post } from '@nestjs/common';
+import { PrismaService } from 'src/database/prisma.service';
+
+//GET POST PUT DELETE = RESTFul
+
+@Controller('api/users')
+export class UsersController {
+    constructor(
+        private readonly prismaService: PrismaService,        
+    ) { }
+
+    @Post()
+    async create() {
+        const userCreated = await this.prismaService.user.create({
+            data: {
+                name: 'João da Silva',
+                email: 'joaozin@jojo.com'
+            }
+        })
+        return {
+            message: 'Usuário criado com sucesso!',
+            data: userCreated
+        }
+    }
+}
